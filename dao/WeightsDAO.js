aca2973e9f8fe3eb6a8a86fb60dc837a5ed46b50
Oscar Dominguez <oscardc@gmx.com>
2015-11-06 16:54:07 +0100

    WeightsDAO modifications
    Replaced insert for upsert, added getWeightByDay

--- dao/WeightsDAO.js
+++ dao/WeightsDAO.js
@@ -1,6 +1,7 @@
 /* The WeightsDAO must be constructed with a connected database object */
 function WeightsDAO(db) {
     'use strict';
+    const LIMIT = 10;
 
     /* If this constructor is called without the 'new' operator, 'this' points
      * to the global object. Log a warning and call it correctly. */
@@ -11,90 +12,43 @@ function WeightsDAO(db) {
 
     const clWeight = db.collection('weight');
 
-    this.insertWeight = function (user, year, month, day, weight, callback) {
+
+    this.insertWeight = function(user, year, month, day, weight, callback) {
         console.log('inserting weight entry for', user, 'data:', year, month, day, weight);
 
-        // fix up the permalink to not include whitespace
-
-        // Build a new post
-        var entry = {
-            'user': user,
-            'date': new Date(year, month, day),
+        const query = {
+            '_id': { // note: this id limits to have only ONE weight entry per user/day
+                'user': user,
+                'date': new Date(year, month, day),
+            }
+        };
+        const update = {
             'weight': weight
+        };
+        const options = {
+            'upsert': true
         };
 
         // now insert the post
-        clWeight.insert(entry, function (err, result) {
-            if (err) return callback(err, null);
-            callback(err, result);
+        clWeight.update(query, update, options, function (err, result) {
+            if (err) return callback(err);
+            callback(err, 'weight ' + (result.nModified===1? 'updated':'inserted' ));
         });
     };
 
-    this.getPosts = function(num, callback) {
-        posts.find().sort('date', -1).limit(num).toArray(function(err, items) {
-            'use strict';
+    this.getWeightByDay = function(user, year, month, day, callback) {
+        const _id = {
+            'user': user,
+            'date': new Date(year, month, day)
+        };
+        clWeight.findOne(_id, function(err, weights) {
+            if (err) return callback(err);
+            console.log('Found ' + weights );
 
-            if (err) return callback(err, null);
-
-            console.log('Found ' + items.length + ' posts');
-
-            callback(err, items);
+            callback(err, weights);
         });
-    }
+    };
 
-    this.getPostsByTag = function(tag, num, callback) {
-        posts.find({ tags : tag }).sort('date', -1).limit(num).toArray(function(err, items) {
-            'use strict';
-
-            if (err) return callback(err, null);
-
-            console.log('Found ' + items.length + ' posts');
-
-            callback(err, items);
-        });
-    }
-
-    this.getPostByPermalink = function(permalink, callback) {
-        posts.findOne({'permalink': permalink}, function(err, post) {
-            'use strict';
-
-            if (err) return callback(err, null);
-
-            // XXX: Look here for final exam to see where we store 'num_likes'
-
-            // fix up likes values. set to zero if data is not present
-            if (typeof post.comments === 'undefined') {
-                post.comments = [];
-            }
-
-            // Each comment document in a post should have a 'num_likes' entry, so we have to
-            // iterate all the comments in the post to make sure that is the case
-            for (var i = 0; i < post.comments.length; i++) {
-                if (typeof post.comments[i].num_likes === 'undefined') {
-                    post.comments[i].num_likes = 0;
-                }
-                post.comments[i].comment_ordinal = i;
-            }
-            callback(err, post);
-        });
-    }
-
-    this.addComment = function(permalink, name, email, body, callback) {
-        var comment = {'author': name, 'body': body}
-
-        if (email != '') {
-            comment['email'] = email
-        }
-
-        posts.update({'permalink': permalink}, {'$push': {'comments': comment}}, function(err, numModified) {
-            'use strict';
-
-            if (err) return callback(err, null);
-
-            callback(err, numModified);
-        });
-    }
-
-}
+};
 
 module.exports.WeightsDAO = WeightsDAO;
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict';
-var express = require('express');
-var MongoClient = require('mongodb').MongoClient;
-var WeightsDAO = require('./dao/WeightsDAO').WeightsDAO;
+const app = require('express')();
+const MongoClient = require('mongodb').MongoClient;
+const WeightsDAO = require('./dao/WeightsDAO').WeightsDAO;  // DAO for weight data
+//const routes = require('./routes'); // Routes for the application
 
 MongoClient.connect('mongodb://mdbuser123:mdbuser123@ds047504.mongolab.com:47504/tracker', function(err, db) {
     if(err) throw err;
+    function simpleHandler(err, doc){
 
-
+    };
+    /* TODO: delete these testing querys */
     var query = { 'user' : 'oscardc' };
     var projection = {
         '_id': 0,
@@ -15,12 +18,18 @@ MongoClient.connect('mongodb://mdbuser123:mdbuser123@ds047504.mongolab.com:47504
     };
     db.collection('weight').find(query, projection).toArray(function(err, docs){
         if(err) throw err;
-        console.dir('find query OK', docs);
+        console.log('find query OK', docs);
     });
-
     var weights = new WeightsDAO(db);
     weights.insertWeight('oscardc', 2015, 11, 6, 68, function(err, doc){
         if(err) throw err;
-        console.log('insert query OK', doc);
+        console.log('TEST: insert OK - ', doc);
     });
+    weights.getWeightByDay('oscardc', 2015, 11, 6, function(err, doc){
+        if(err) throw err;
+        console.log('TEST: getWeightByDay OK', doc);
+    });
+    /* TODO END */
+
+    //routes(app, db);
 });
